784cfee832bf558c8242dd12691075d5bc6c39ae
Andrii Makovii <s17386@pjwstk.edu.pl>
2022-10-23 22:36:57 +0200

    first part before brake

---
+++ WebApplication1/Models/Student.cs
@@ -0,0 +1,13 @@
+namespace WebApplication1.Models;
+
+public class Student
+{
+    public int IdStudent { get; set; }
+    public string FirstName { get; set; }
+    public string LastName  { get; set; }
+    public string Email { get; set; }
+    public string IndexNumber { get; set; }
+    public string Address { get; set; }
+    
+           
+}
